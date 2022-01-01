90b32e3f4b4c5563e944ab085840491054594743
Adam Celarek <git_commits@xibo.at>
2022-01-01 22:09:19 +0100

    abort if the number of tiles is very large.

--- terrainlib/Tiler.cpp
+++ terrainlib/Tiler.cpp
@@ -1,5 +1,8 @@
 #include "Tiler.h"
 
+#include "Exception.h"
+
+
 Tiler::Tiler(const ctb::Grid& grid, const ctb::CRSBounds& bounds, Border border) :
     m_grid(grid), m_bounds(bounds),
     m_border_south_east(border)
@@ -9,12 +12,10 @@ Tiler::Tiler(const ctb::Grid& grid, const ctb::CRSBounds& bounds, Border border)
 
 std::vector<Tile> Tiler::generateTiles(ctb::i_zoom zoom_level) const
 {
+
   const auto sw = m_grid.crsToTile(m_bounds.getLowerLeft(), zoom_level);
   const auto epsilon = m_grid.resolution(zoom_level) / 100;
   const auto ne = m_grid.crsToTile(m_bounds.getUpperRight() - epsilon, zoom_level);
-//  const auto bounds = ctb::TileBounds(sw, ne);
-
-  auto currentTile = sw;
 
   std::vector<Tile> tiles;
   for (auto ty = sw.y; ty <= ne.y; ++ty) {
@@ -25,6 +26,13 @@ std::vector<Tile> Tiler::generateTiles(ctb::i_zoom zoom_level) const
       ctb::i_tile grid_size = m_grid.tileSize();
       ctb::i_tile tile_size = grid_size + unsigned(m_border_south_east);
       tiles.emplace_back(point, zoom_level, srs_bounds, grid_size, tile_size);
+
+      if (tiles.size() >= 100'000'000)
+        // think about creating an on the fly tile generator. storing so many tiles takes a lot of memory.
+        throw Exception("Setting the zoom level so higher is probably not a good idea. This would generate more than 100 million tiles. "
+                        "I'm aborting. If you really need this, then that means that the future is bright. But you'll have to edit the code..\n"
+                        "      .   .     \n     / \\_/ \\    \n    | O  O |   \n    | ~V~  |  _\n     ~_ _ /  // \n    /     \\ //  \n"
+                        "    |  ||  |/  \n    | /  \\ |   \n    ||    ||   \n               \n");
     }
   }
 
