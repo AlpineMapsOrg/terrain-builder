e4863f0aec77d80031fa9c49cd528471c9007bd6
Adam Celarek <git_commits@xibo.at>
2022-01-23 19:29:00 +0100

    compile fix for g++ 11

--- unittests/image.cpp
+++ unittests/image.cpp
@@ -17,7 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 
-#include <bits/ranges_algo.h>
 #include <catch2/catch.hpp>
 #include <algorithm>
 
