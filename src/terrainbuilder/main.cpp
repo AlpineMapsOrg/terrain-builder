a3372713e2e8492dd94bd62fcfb2f5bc74f54123
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2023-10-05 15:41:51 +0200

    remove quantized mesh support (unit tests were failing, and iirc it never worked correctly)

--- src/terrainbuilder/main.cpp
+++ src/terrainbuilder/main.cpp
@@ -6,7 +6,6 @@
 #include "ParallelTiler.h"
 #include "TileHeightsGenerator.h"
 #include "alpine_raster.h"
-#include "cesium_tin_terra.h"
 #include "ctb/Grid.hpp"
 
 int main()
@@ -15,12 +14,11 @@ int main()
     //  const std::string output_path = "/home/madam/rajaton/tiles/atb_terrain/";
     ////  const auto generator = alpine_raster::make_generator("./test_tiles/", "/home/madam/valtava/raw/Oe_2020/OeRect_01m_gs_31287.img", ctb::Grid::Srs::SphericalMercator, Tiler::Scheme::SlippyMap, Tiler::Border::No);
     ////  generator.process({16, 16});
-    //  const auto generator = cesium_tin_terra::make_generator(input_raster, output_path, ctb::Grid::Srs::WGS84, Tiler::Scheme::Tms, Tiler::Border::Yes);
     //  generator.process({0, 5}, true, true);
     //  generator.process({6, 16}, true, false);
 
-        const std::string input_raster = "/home/madam/valtava/raw/Oe_2020/OeRect_01m_gs_31287.img";
-//    const std::string input_raster = "/home/madam/valtava/raw/vienna/innenstadt_gs_1m_mgi.tif";
+    const std::string input_raster = "/home/madam/valtava/raw/Oe_2020/OeRect_01m_gs_31287.img";
+    //    const std::string input_raster = "/home/madam/valtava/raw/vienna/innenstadt_gs_1m_mgi.tif";
     const std::string output_path = "/home/madam/valtava/tiles/alpine_png2";
     //  const auto generator = alpine_raster::make_generator("./test_tiles/", "/home/madam/valtava/raw/Oe_2020/OeRect_01m_gs_31287.img", ctb::Grid::Srs::SphericalMercator, Tiler::Scheme::SlippyMap, Tiler::Border::No);
     //  generator.process({16, 16});
